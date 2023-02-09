5dfe9940b85b47a1cc31b5705d63749b90cf1140
Everett Gaius Vergara <evergara@shinra.com.ph>
2023-02-09 18:35:47 +0800

    added svton test

---
+++ svton.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string_view>
+#include "common_fn.hpp"
+
+
+auto main() -> int 
+{
+    std::string_view sv = "100.34";
+
+    auto fp = eg::svton<double>(sv);
+
+    std::cout << fp.value() << std::endl;
+}
